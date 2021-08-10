527576d64376694d7656b826d4b4db545e8ca4d7
Chase Rivera <achaserivera@gmail.com>
2021-08-09 17:12:09 -0700

    added delete route to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,6 +107,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const results = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!results) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json('Product Deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
