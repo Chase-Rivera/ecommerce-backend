782ed45a4d25bb3f5e080569bf8ccb6be0c3bd32
Chase Rivera <achaserivera@gmail.com>
2021-08-09 17:20:26 -0700

    added post route to create new tag to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const results = await Tag.create(req.body);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
